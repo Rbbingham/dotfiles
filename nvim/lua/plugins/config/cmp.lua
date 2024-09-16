2cd986cb6f1cb50ccf4903284f4881272218fd5d
Robert Bingham <rbbingham1893@gmail.com>
2024-09-15 21:04:09 -0400

    lower priority for words in buffer

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -112,8 +112,8 @@ local options = {
 		sources = {
 			{ name = "nvim_lsp", priority = 1000 },
 			{ name = "luasnip", priority = 750 },
-			{ name = "buffer", priority = 500 },
-			{ name = "path", priority = 250 },
+			{ name = "path", priority = 500 },
+			{ name = "buffer", priority = 250 },
 		},
 	}),
 
