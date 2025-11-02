c0997f6c8e456bfbb2e91e589ca03d63c288f807
Robert Bingham <rbbingham1893@gmail.com>
2025-11-02 18:32:54 -0500

    refactor(options): remove redundant options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,8 +3,6 @@ local options = {
 	------------------------------------------------------------------------
 	--                         General settings                           --
 	------------------------------------------------------------------------
-	history = 100,                       -- lines to remember
-	mouse = "a",                         -- allow mouse
 	timeoutlen = 400,                    -- time delay on <Leader> key
 	updatetime = 250,                    -- update time
 	nrformats = "hex,bin,unsigned",      -- set inc/dec
@@ -12,22 +10,20 @@ local options = {
 	clipboard = "unnamedplus",           -- sync clipboard between OS and neovim
 	cursorline = true,                   -- highlight text line of cursor
 	laststatus = 3,                      -- global statusline
+	winborder = "single",                -- border around floating window
 
 	------------------------------------------------------------------------
 	--                         Files and backup                           --
 	------------------------------------------------------------------------
 	swapfile = false,                    -- Disable swap files
-	backup = false,                      -- No backup (use git)
 	writebackup = false,                 -- Prevents automatic write backup
 	fileencoding = "utf-8",              -- encoding written to file
 	fileformats = "unix",                -- use unix as the standard file type
-	autoread = true,                     -- autoread a file when it is changed
 	undofile = true,                     -- enable persistant undo
 
 	------------------------------------------------------------------------
 	--                         Buffers management                         --
 	------------------------------------------------------------------------
-	hidden = true,                       -- buffer hidden when it is abandoned
 	switchbuf = "useopen,usetab,newtab", -- set behavior when switching between buffers
 	showtabline = 2,                     -- always show tab page
 
@@ -40,12 +36,9 @@ local options = {
 	------------------------------------------------------------------------
 	--                         Indentation tabs                           --
 	------------------------------------------------------------------------
-	autoindent = true,                   -- enable autoindent
 	smartindent = true,                  -- enable smartindent
 	preserveindent = true,               -- preserve indent as much as possible
 	copyindent = true,                   -- copy the previous indentation on autoindenting
-	expandtab = false,                   -- use tabs instead of spaces
-	smarttab = true,                     -- be smart when using tabs
 	shiftwidth = 2,                      -- number of spaces to use for indent
 	tabstop = 2,                         -- number of spaces that a <TAB> counts for
 	softtabstop = 2,                     -- number of spaces that a <TAB> counts for while performing editing ops
@@ -56,7 +49,6 @@ local options = {
 	--                         Moving around lines                        --
 	------------------------------------------------------------------------
 	whichwrap = "<,>,h,l",               -- specify commands to wrap to another line
-	wrap = true,                         -- wrap lines into the window
 	linebreak = true,                    -- don't break words
 	breakindent = true,                  -- enable wrapped lines to continue indent
 	showbreak = "↳ ",                    -- symbol to show on wrapped lines
@@ -69,14 +61,10 @@ local options = {
 	------------------------------------------------------------------------
 	--                         Search, vimgrep, and grep                  --
 	------------------------------------------------------------------------
-	hlsearch = true,                     -- highlight search results
-	incsearch = true,                    -- show where the pattern, as typed, matches
 	inccommand = "split",                -- split window preview for substitution
-	wrapscan = true,                     -- search, wrap around the end of the buffer
 	ignorecase = true,                   -- ignore case when searching
 	infercase = true,                    -- infer cases in keyword completion
 	smartcase = true,                    -- try to be smart about case
-	magic = true,                        -- allow regular expressions
 }
 
 local global = {
@@ -85,8 +73,6 @@ local global = {
 	loaded_ruby_provider = 0,            -- disable ruby provider
 	loaded_node_provider = 0,            -- disable node provider
 	loaded_perl_provider = 0,            -- disable perl provider
-	load_doxygen_syntax = 1,
-	doxygen_enhanced_color = 1,
 }
 -- stylua: ignore end
 
