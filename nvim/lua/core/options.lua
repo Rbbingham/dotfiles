bbda907584aaf1bb950055884cbddea80c02d6f6
Robert Bingham <rbbingham1893@gmail.com>
2025-11-07 18:53:40 -0500

    refactor(options): add options virtualedit & completeopt/sort options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,76 +3,78 @@ local options = {
 	------------------------------------------------------------------------
 	--                         General settings                           --
 	------------------------------------------------------------------------
-	timeoutlen = 400,                    -- time delay on <Leader> key
-	updatetime = 250,                    -- update time
-	nrformats = "hex,bin,unsigned",      -- set inc/dec
-	termguicolors = true,                -- enable 24-bit RGB color in the TUI
-	clipboard = "unnamedplus",           -- sync clipboard between OS and neovim
-	cursorline = true,                   -- highlight text line of cursor
-	laststatus = 3,                      -- global statusline
-	winborder = "single",                -- border around floating window
+	clipboard = "unnamedplus",              -- sync clipboard between OS and Neovim
+	completeopt = "menuone,noselect,fuzzy", -- customize insert mode completion
+	cursorline = true,                      -- highlight text line of cursor
+	laststatus = 3,                         -- global statusline
+	nrformats = "hex,bin,unsigned",         -- set inc/dec
+	termguicolors = true,                   -- enable 24-bit RGB color in the TUI
+	timeoutlen = 400,                       -- time delay on <Leader> key
+	updatetime = 250,                       -- update time
+	virtualedit = "block",                  -- allow cursor to go past end of line in visual block mode
 
 	------------------------------------------------------------------------
 	--                         Files and backup                           --
 	------------------------------------------------------------------------
-	swapfile = false,                    -- Disable swap files
-	writebackup = false,                 -- Prevents automatic write backup
-	fileencoding = "utf-8",              -- encoding written to file
-	fileformats = "unix",                -- use unix as the standard file type
-	undofile = true,                     -- enable persistant undo
+	fileencoding = "utf-8",                 -- encoding written to file
+	fileformats = "unix",                   -- use Unix as the standard file type
+	swapfile = false,                       -- disable swap files
+	undofile = true,                        -- enable persistent undo
+	writebackup = false,                    -- prevents automatic write backup
 
 	------------------------------------------------------------------------
 	--                         Buffers management                         --
 	------------------------------------------------------------------------
-	switchbuf = "useopen,usetab,newtab", -- set behavior when switching between buffers
-	showtabline = 2,                     -- always show tab page
+	showtabline = 2,                        -- always show tab page
+	switchbuf = "useopen,usetab,newtab",    -- set behavior when switching between buffers
 
 	------------------------------------------------------------------------
 	--                         Multiple windows                           --
 	------------------------------------------------------------------------
-	splitbelow = true,                   -- put split window below current one
-	splitright = true,                   -- put split window to the right of current one
+	splitbelow = true,                      -- put split window below current one
+	splitright = true,                      -- put split window to the right of current one
+	winborder = "single",                   -- border around floating window
 
 	------------------------------------------------------------------------
 	--                         Indentation tabs                           --
 	------------------------------------------------------------------------
-	smartindent = true,                  -- enable smartindent
-	preserveindent = true,               -- preserve indent as much as possible
-	copyindent = true,                   -- copy the previous indentation on autoindenting
-	shiftwidth = 2,                      -- number of spaces to use for indent
-	tabstop = 2,                         -- number of spaces that a <TAB> counts for
-	softtabstop = 2,                     -- number of spaces that a <TAB> counts for while performing editing ops
-	list = true,                         -- don't show tabs
-	listchars = "tab:┊ ,trail:¬",        -- set tab and end-of-line characters
+	copyindent = true,                      -- copy the previous indentation on autoindenting
+	list = true,                            -- don't show tabs
+	listchars = "tab:┊ ,trail:¬",           -- set tab and end-of-line characters
+	preserveindent = true,                  -- preserve indent as much as possible
+	shiftwidth = 2,                         -- number of spaces to use for indent
+	smartindent = true,                     -- enable smartindent
+	softtabstop = 2,                        -- number of spaces that a <TAB> counts for while performing editing ops
+	tabstop = 2,                            -- number of spaces that a <TAB> counts for
 
 	------------------------------------------------------------------------
 	--                         Moving around lines                        --
 	------------------------------------------------------------------------
-	whichwrap = "<,>,h,l",               -- specify commands to wrap to another line
-	linebreak = true,                    -- don't break words
-	breakindent = true,                  -- enable wrapped lines to continue indent
-	showbreak = "↳ ",                    -- symbol to show on wrapped lines
-	colorcolumn = "80",                  -- column at 80 width
-	number = true,                       -- print line number in front of line
-	numberwidth = 2,                     -- minimal number of columns to use for line number
-	relativenumber = true,               -- show line number relative to cursor
-	conceallevel = 2,                    -- concealed text is hidden unless it has a custom replacement char
+	breakindent = true,                     -- enable wrapped lines to continue indent
+	colorcolumn = "80",                     -- column at 80 width
+	conceallevel = 2,                       -- concealed text is hidden unless it has a custom replacement char
+	linebreak = true,                       -- don't break words
+	number = true,                          -- print line number in front of line
+	numberwidth = 2,                        -- minimal number of columns to use for line number
+	relativenumber = true,                  -- show line number relative to cursor
+	showbreak = "↳ ",                       -- symbol to show on wrapped lines
+	whichwrap = "<,>,h,l",                  -- specify commands to wrap to another line
 
 	------------------------------------------------------------------------
 	--                         Search, vimgrep, and grep                  --
 	------------------------------------------------------------------------
-	inccommand = "split",                -- split window preview for substitution
-	ignorecase = true,                   -- ignore case when searching
-	infercase = true,                    -- infer cases in keyword completion
-	smartcase = true,                    -- try to be smart about case
+	ignorecase = true,                      -- ignore case when searching
+	inccommand = "split",                   -- split window preview for substitution
+	infercase = true,                       -- infer cases in keyword completion
+	smartcase = true,                       -- try to be smart about case
 }
 
 local global = {
-	mapleader = ",",                     -- set the leader key
-	maplocalleader = ",",                -- set the local leader key
-	loaded_ruby_provider = 0,            -- disable ruby provider
-	loaded_node_provider = 0,            -- disable node provider
-	loaded_perl_provider = 0,            -- disable perl provider
+	loaded_node_provider = 0,               -- disable node provider
+	loaded_perl_provider = 0,               -- disable Perl provider
+	loaded_ruby_provider = 0,               -- disable ruby provider
+	mapleader = ",",                        -- set the leader key
+	maplocalleader = ",",                   -- set the local leader key
 }
 -- stylua: ignore end
 
