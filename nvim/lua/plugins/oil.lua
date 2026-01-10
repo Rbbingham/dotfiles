0130156d62bdd08e2b7de70ecc14c0d8cedfa339
Robert Bingham <rbbingham1893@gmail.com>
2026-01-10 11:05:57 -0500

    feat(oil): add new keybindings for working with files in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,6 +5,24 @@ return {
 		require("oil").setup({
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
+			keymaps = {
+				["~"] = "<cmd>edit $HOME<CR>",
+				["<leader>x"] = {
+					"actions.open_cmdline",
+					opts = {
+						shorten_path = true,
+					},
+					desc = "Open the command line with the current file as an argument",
+				},
+				["<leader>X"] = {
+					"actions.open_cmdline",
+					opts = {
+						shorten_path = true,
+						modify = ":h",
+					},
+					desc = "Open the command line with the current directory as an argument",
+				},
+			},
 			view_options = {
 				show_hidden = true,
 				is_always_hidden = function(name, _)
