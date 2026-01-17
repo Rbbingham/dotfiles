cc460e60c204af1b0017cfe8aa3b47eeb324185a
Robert Bingham <rbbingham1893@gmail.com>
2026-01-16 23:12:44 -0500

    fix(treesitter): lock nvim-treesitter to the master branch

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@ return {
 	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	branch = "master",
 	event = { "BufReadPost", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
